62a538c6c587faa4d40407e564175a1083ea67ca
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-07-03 15:54:42 +0200

    Add some validations in Both modules and chnages in index view

--- app/models/truck_tour.rb
+++ app/models/truck_tour.rb
@@ -1,3 +1,10 @@
 class TruckTour < ApplicationRecord
   belongs_to :truck_fleet
+
+  # page should exist to enter a truck tour which is identified by a unique name
+  validates_uniqueness_of :name
+
+  #validates some columns
+  validates :distance_in_kilometer, :tour_start_date, :tour_end_date, presence: true
+
 end
